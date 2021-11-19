a63340afb632932fc5e2d24dc0a1cdd14d91c71c
Fabrizio Onorio <onoriofabrizio@gmail.com>
2021-11-19 22:07:34 +0100

    fixed map

--- app.js
+++ app.js
@@ -17,3 +17,42 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+ mapboxgl.accessToken = 'pk.eyJ1IjoiZmFicml6aW8tb25vcmlvIiwiYSI6ImNrdzZpdGhuaDBva3Ayb3JobzA2dXRkeWsifQ.EDIljUREdTleOEGRQH5i8A';
+
+  const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [10.209954261779785, 45.54516220092773]
+        },
+        'properties': {
+          'title': 'Mapbox',
+          'description': 'Studio di Marco'
+        }
+      }
+    ]
+  };
+
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [10.209954261779785, 45.54516220092773],
+    zoom: 14
+  });
+
+  // add markers to map
+  for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // make a marker for each feature and add it to the map
+    new mapboxgl.Marker(el)
+      .setLngLat(feature.geometry.coordinates)
+      .addTo(map);
+  }
